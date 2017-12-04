79bbd2fb0fc12c411b02db41c3a842ce0bbcad8c
Connor <czoop@me.com>
2017-12-04 14:49:07 -0800

    Added more dialogue

--- src/Magpie.java
+++ src/Magpie.java
@@ -1,5 +1,19 @@
 public class Magpie {
 	private String name;
+	/*
+	 * 0 = Family
+	 * 1 = Life
+	 * 2 = Pets
+	 * 3 = Goods
+	 * 4 = Bads
+	 * 5 = Queries
+	 */
+	private String[][] keyWords = {{"family", "mother", "father", "brother", "sister", "mom", "dad"},
+									{"school", "work", "life", "issues", "worries"}, 
+									{"dog", "cat", "bird", "snake", "pet", "pets", "fish", "dogs", "cats", "birds"},
+									{"fine", "good", "well", "okay", "better"},
+									{"bad", "poorly", "awful", "sad"},
+									{"what is", "how does", "does", "can you", "please get"}};
 	
 	public Magpie() {
 		this.name = "";
@@ -20,23 +34,57 @@ public class Magpie {
 	{
 		statement = statement.toLowerCase();
 		
-		String familyMember = checkFor(statement, new String[] {"mother", "father", "brother", "sister", "mom", "dad"});
-		if (!familyMember.equals(""))
+		if (checkFor(statement, keyWords[0]))
 		{
+			String familyMember = getWord(statement, keyWords[0]);
 			return "Please tell me more about your " + familyMember + ".";
+		}
+		else if (checkFor(statement, keyWords[1]))
+		{
+			String word = getWord(statement, keyWords[1]);
+			return "Please do tell me a little bit more about your " + word + ".";
+		}
+		else if (checkFor(statement, keyWords[2]))
+		{
+			String pet = getWord(statement, keyWords[2]);
+			return "How is your " + pet + "doing?";
+		}
+		else if (checkFor(statement, keyWords[3]))
+		{
+			return "That's good!";
+		}
+		else if (checkFor(statement, keyWords[4])) 
+		{
+			return "I'm sorry. I hope it gets better.";
+		}
+		//Queries
+		else if (checkFor(statement, keyWords[5]))
+		{
+			return "I'm afraid I'm not smart enough to do that. I'm sorry.";
+		}
+		else if (checkFor(statement, new String[] {"magpie"}))
+		{
+			return "How are you?";
 		}
 		else
 		{
 			return getRandResponce();
 		}
-		
-		if (checkFor(statement, new String[] {"magpie"}).equals(""))
-		{
-			return "How are you?";
-		}
 	}
 	
-	private String checkFor(String statement, String[] words) 
+	private boolean checkFor(String statement, String[] words) 
+	{
+		for (String word : words) 
+		{
+			if (statement.indexOf(word) >= 0)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private String getWord(String statement, String[] words) 
 	{
 		for (String word : words) 
 		{
@@ -45,6 +93,11 @@ public class Magpie {
 				return word;
 			}
 		}
+		return "ERROR";
+	}
+	
+	private String randResponce(String[] statements)
+	{
 		return "";
 	}
 	
